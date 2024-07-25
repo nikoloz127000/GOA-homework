354a7163fe064cca50852b5a1a5a9341cc573d42
nikoloz127000 <nicksmith127000@gmail.com>
2024-07-25 19:59:57 +0400

    fourth commit

---
+++ level 09/homework/homework.py
@@ -0,0 +1,43 @@
+num = int(input("please enter the number:"))
+
+num1 = int(input("please enter the number:"))
+
+print(num + num1)
+
+print(num - num1)
+
+print(num * num1)
+
+print(num / num1)
+
+print(num // num1)
+
+print(num ** num1)
+
+
+
+
+
+age = int(input("please enter your age:"))
+
+
+print(age + 10)
+
+
+
+
+
+
+stringa = "Hello"
+
+integera = 5
+
+floata = 2.5
+
+print(type(stringa))
+
+print(type(integera))
+
+print(type(floata))
+      
+
---
+++ level 11/homework/homework.py
@@ -0,0 +1,36 @@
+Email = (input("Please  enter your Email:"))
+
+Email_Password = (input("please enter your password:"))
+
+
+print(Email)
+
+print(Email_Password)
+
+
+
+
+#2 homework
+
+#guess my age!
+
+
+age =  int(input("Guess my age:"))
+age1 =  int(input("Guess my age:"))
+
+
+
+print(age)
+print(age1)
+
+if age == 10:
+    print("bingo you are right")
+
+else:
+    print("try again")
+
+if age1 == 10:
+    print("bingo you are right")
+
+else:
+    print("you lose")
