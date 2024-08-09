fab69c45fa13112d30f67ed2b927d333d5f09c07
nikoloz127000 <nicksmith127000@gmail.com>
2024-08-09 16:15:44 +0400

    seventh commit

---
+++ Level 21/home.py
@@ -0,0 +1,11 @@
+#1
+
+your_word = int(input("please enter number: "))
+
+guess = int(input("please enter number: "))
+
+while your_word != guess:
+    guess = int(input("please try again: "))
+    
+
+
